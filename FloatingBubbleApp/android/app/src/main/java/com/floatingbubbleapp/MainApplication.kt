9ffe4af9f1b9a8c23ddac2ca26dd30aec7faf44d
krzysiekpodk <krzysiek.podkanowicz@gmail.com>
2024-05-26 19:47:25 +0200

    Update MainApplication.kt

--- FloatingBubbleApp/android/app/src/main/java/com/floatingbubbleapp/MainApplication.kt
+++ FloatingBubbleApp/android/app/src/main/java/com/floatingbubbleapp/MainApplication.kt
@@ -3,30 +3,28 @@ package com.floatingbubbleapp
 import android.app.Application
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
 import com.facebook.react.shell.MainReactPackage
 import com.facebook.soloader.SoLoader
-import java.util.*
+import java.util.Arrays
 
 class MainApplication : Application(), ReactApplication {
-    // Override the ReactNativeHost as a read-only property.
-    override val reactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
-        override fun getUseDeveloperSupport(): Boolean {
-            return BuildConfig.DEBUG
-        }
+
+    private val mReactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
         override fun getPackages(): List<ReactPackage> {
-            // Ensure the list includes all necessary packages.
             return listOf(
                 MainReactPackage(),
-                FloatingBubblePackage()  // Your custom package
+                FloatingBubblePackage()  // Ensure this is properly defined and imported
             )
         }
 
-        override fun getJSMainModuleName(): String {
-            // Point this to your entry JavaScript file, typically "index"
-            return "index"
-        }
+        override fun getJSMainModuleName(): String = "index"
     }
+
+    override val reactNativeHost: ReactNativeHost
+        get() = mReactNativeHost
 
     override fun onCreate() {
         super.onCreate()
